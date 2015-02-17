2819c432846e27cb274f2ed4ac2c6828629bf312
NeoRazorX <neorazorx@gmail.com>
2015-02-17 18:16:14 +0100

    Añadida corrección para que curl ya pueda descargar archivos por HTTPS en sistemas windows.

--- controller/admin_home.php
+++ controller/admin_home.php
@@ -726,6 +726,8 @@ y las adquisiciones de bienes y servicios.'
          curl_setopt($ch, CURLOPT_URL, $url);
          curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
          curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
          $data = curl_exec($ch);
          curl_close($ch);
          return $data;
--- updater.php
+++ updater.php
@@ -98,6 +98,8 @@ function curl_get_contents($url)
       curl_setopt($ch, CURLOPT_URL, $url);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
       $data = curl_exec($ch);
       curl_close($ch);
       return $data;
