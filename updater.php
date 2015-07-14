cdaeea2172c7efabcacfa828cdfbe56a939f1b0b
carlos <carlos@m5a97>
2015-07-15 00:36:06 +0200

    Pequeña corrección.

--- updater.php
+++ updater.php
@@ -327,7 +327,7 @@ class fs_updater
       while (false !== ( $file = readdir($dir))) {
          if (( $file != '.' ) && ( $file != '..' )) {
             if (is_dir($src . '/' . $file)) {
-               recurse_copy($src . '/' . $file, $dst . '/' . $file);
+               $this->recurse_copy($src . '/' . $file, $dst . '/' . $file);
             } else {
                copy($src . '/' . $file, $dst . '/' . $file);
             }
