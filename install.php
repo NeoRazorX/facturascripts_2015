12f07dcbe29b159dcb6ee86ce4b97c367597c1d8
carlos <carlos@m5a97>
2017-01-03 10:39:05 +0100

    Añadida licencia al instalador, además de actualizar el año del copyright.

--- install.php
+++ install.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ * This file is part of FacturaScripts
+ * Copyright (C) 2013-2017  Carlos Garcia Gomez  neorazorx@gmail.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 $nombre_archivo = "config.php";
 error_reporting(E_ALL);
@@ -837,7 +854,7 @@ $system_info = str_replace('"', "'", $system_info);
          <div class="col-sm-12 text-center">
             <hr/>
             <small>
-               © 2013-2016 <a target="_blank" href="https://www.facturascripts.com">FacturaScripts</a>
+               &COPY; 2013-2017 <a target="_blank" href="https://www.facturascripts.com">FacturaScripts</a>
             </small>
          </div>
       </div>
