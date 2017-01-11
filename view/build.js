75d7a5b2140da932c33e7bef7205fa921dbdcf4a
dialmedu <diegomesa.1414@gmail.com>
2017-01-11 01:12:40 -0500

    Corregida version de jquery autocomplete, repo de @devbrige
    - El autocompletado no funcionaba, se estaba usando un archivo de un repositorio incorrecto.
    - En la rama master se usa una version de jquery.autocomplete obtenida del repo de @devbridge

--- view/build.js
+++ view/build.js
@@ -6,7 +6,7 @@ buildify()
         nodePath + 'jquery/dist/jquery.js',
         nodePath + 'bootstrap-sass/assets/javascripts/bootstrap.js',
         nodePath + 'bootstrap-datepicker/dist/js/bootstrap-datepicker.js',
-        nodePath + 'jquery-autocomplete/jquery.autocomplete.js',
+        nodePath + 'devbridge-autocomplete/dist/jquery.autocomplete.min.js',
         nodePath + 'jquery-validation/dist/jquery.validate.js',
         nodePath + 'jquery-ui/ui/widget.js',
         'js/base.js'
