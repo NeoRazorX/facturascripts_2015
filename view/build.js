26b1b2cf84564f0028290e16400227a2d81d0fd0
dialmedu <diegomesa.1414@gmail.com>
2017-01-12 22:32:52 -0500

    Actualizada buils JS y CSS para FacturaScripts
    - Tras recatar eliminaciones de js de facturascripts ,se actualiza el buld.js , se copilan los archivos js y css base para usar facturascripts

--- view/build.js
+++ view/build.js
@@ -5,11 +5,12 @@ buildify()
     .concat([
         nodePath + 'jquery/dist/jquery.js',
         nodePath + 'bootstrap-sass/assets/javascripts/bootstrap.js',
-        nodePath + 'bootstrap-datepicker/dist/js/bootstrap-datepicker.js',
         nodePath + 'devbridge-autocomplete/dist/jquery.autocomplete.min.js',
         nodePath + 'jquery-validation/dist/jquery.validate.js',
         nodePath + 'jquery-ui/ui/widget.js',
-        'js/base.js'
+        'js/base.js',
+        'js/bootstrap-datepicker.js',
+        'js/jquery.ui.shake.js'
     ])
     .uglify()
     .save('js/build.min.js');
