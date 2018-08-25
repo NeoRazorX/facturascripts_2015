976e7fd1a8fe0274fd361fdb7a49a5172abd62dd
NeoRazorX <neorazorx@gmail.com>
2018-08-25 23:11:27 +0200

    - Refactorizada la clase fs_login y mejorado el de fs_ip_filter. - Ahora se tiene en cuenta cada intento de login para el control por IP. Antes solamente se tenía en cuenta si el nick existía.

--- base/fs_login.php
+++ base/fs_login.php
@@ -10,11 +10,11 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 require_once 'base/fs_ip_filter.php';
 
@@ -26,13 +26,39 @@ require_once 'base/fs_ip_filter.php';
 class fs_login
 {
 
+    /**
+     *
+     * @var string
+     */
+    private $ban_message;
+
+    /**
+     *
+     * @var fs_cache
+     */
     private $cache;
+
+    /**
+     *
+     * @var fs_core_log
+     */
     private $core_log;
+
+    /**
+     *
+     * @var fs_ip_filter
+     */
     private $ip_filter;
+
+    /**
+     *
+     * @var fs_user
+     */
     private $user_model;
 
     public function __construct()
     {
+        $this->ban_message = 'Tendrás que esperar ' . fs_ip_filter::BAN_SECONDS . ' segundos antes de volver a intentar entrar.';
         $this->cache = new fs_cache();
         $this->core_log = new fs_core_log();
         $this->ip_filter = new fs_ip_filter();
@@ -41,154 +67,86 @@ class fs_login
 
     public function change_user_passwd()
     {
-        $db_password = filter_input(INPUT_POST, 'db_password');
         $ip = fs_get_ip();
         $nick = filter_input(INPUT_POST, 'user');
+        if ($this->ip_filter->is_banned($ip)) {
+            $this->ip_filter->setAttempt($ip);
+            $this->core_log->new_error('Tu IP ha sido baneada, ' . $nick . '. ' . $this->ban_message);
+            $this->core_log->save('Tu IP ha sido baneada, ' . $nick . '. ' . $this->ban_message);
+            return FALSE;
+        }
+
+        if (!$this->ip_filter->in_white_list($ip)) {
+            $this->core_log->new_error('No puedes acceder desde esta IP, ' . $nick . '.');
+            $this->core_log->save('No puedes acceder desde esta IP, ' . $nick . '.', 'login', TRUE);
+            return FALSE;
+        }
+
         $new_password = filter_input(INPUT_POST, 'new_password');
         $new_password2 = filter_input(INPUT_POST, 'new_password2');
-
-        if ($this->ip_filter->isBanned($ip)) {
-            $this->ip_filter->setAttempt($ip);
-            $this->core_log->new_error('Tu IP ha sido baneada ' . $nick . '. '
-                . 'Tendrás que esperar 10 minutos antes de volver a intentar entrar.');
-        } else if ($new_password != $new_password2) {
-            $this->core_log->new_error('Las contraseñas no coinciden ' . $nick);
-        } else if ($new_password == '') {
-            $this->core_log->new_error('Tienes que escribir una contraseña nueva ' . $nick);
-        } else if ($db_password != FS_DB_PASS) {
-            $this->ip_filter->setAttempt($ip);
-            $this->core_log->new_error('La contraseña de la base de datos es incorrecta ' . $nick);
-        } else {
-            $suser = $this->user_model->get($nick);
-            if ($suser) {
-                $suser->set_password($new_password);
-                if ($suser->save()) {
-                    $this->core_log->new_message('Contraseña cambiada correctamente ' . $nick);
-                } else {
-                    $this->core_log->new_error('Imposible cambiar la contraseña del usuario ' . $nick);
-                }
-            }
+        if ($new_password != $new_password2) {
+            $this->core_log->new_error('Las contraseñas no coinciden, ' . $nick);
+            return FALSE;
         }
+
+        if ($new_password == '') {
+            $this->core_log->new_error('Tienes que escribir una contraseña nueva, ' . $nick);
+            return FALSE;
+        }
+
+        $db_password = filter_input(INPUT_POST, 'db_password');
+        if ($db_password != FS_DB_PASS) {
+            $this->ip_filter->set_attempt($ip);
+            $this->core_log->new_error('La contraseña de la base de datos es incorrecta, ' . $nick);
+            return FALSE;
+        }
+
+        $user = $this->user_model->get($nick);
+        if ($user) {
+            $user->set_password($new_password);
+            if ($user->save()) {
+                $this->core_log->new_message('Contraseña cambiada correctamente para ' . $nick);
+                return TRUE;
+            }
+
+            $this->core_log->new_error('Imposible cambiar la contraseña del usuario ' . $nick);
+        }
+
+        return FALSE;
     }
 
+    /**
+     * 
+     * @param fs_user $controller_user
+     *
+     * @return boolean
+     */
     public function log_in(&$controller_user)
     {
         $ip = fs_get_ip();
         $nick = filter_input(INPUT_POST, 'user');
-        $password = filter_input(INPUT_POST, 'password');
-
-        if ($this->ip_filter->isBanned($ip)) {
-            $this->core_log->new_error('Tu IP ha sido baneada. Tendrás que esperar 10 minutos antes de volver a intentar entrar.');
-            $this->core_log->save('Tu IP ha sido baneada. Tendrás que esperar 10 minutos antes de volver a intentar entrar.', 'login', TRUE);
+        if ($this->ip_filter->is_banned($ip)) {
+            $this->core_log->new_error('Tu IP ha sido baneada, ' . $nick . '. ' . $this->ban_message);
+            $this->core_log->save('Tu IP ha sido baneada, ' . $nick . '. ' . $this->ban_message, 'login', TRUE);
             return FALSE;
         }
 
+        $password = filter_input(INPUT_POST, 'password');
         if ($nick && $password) {
-            if (FS_DEMO) { /// en el modo demo nos olvidamos de la contraseña
-                $this->login_demo($controller_user, $nick);
-            } else {
-                $user = $this->user_model->get($nick);
-                if ($user && $user->enabled) {
-                    /**
-                     * En versiones anteriores se guardaban las contraseñas siempre en
-                     * minúsculas, por eso, para dar compatibilidad comprobamos también
-                     * en minúsculas.
-                     */
-                    if ($user->password == sha1($password) || $user->password == sha1(mb_strtolower($password, 'UTF8'))) {
-                        $user->new_logkey();
-
-                        if (!$user->admin && !$this->ip_filter->inWhiteList($ip)) {
-                            $this->core_log->new_error('No puedes acceder desde esta IP.');
-                            $this->core_log->save('No puedes acceder desde esta IP.', 'login', TRUE);
-                        } else if ($user->save()) {
-                            $this->save_cookie($user);
-                            $controller_user = $user;
-
-                            /// añadimos el mensaje al log
-                            $this->core_log->save('Login correcto.', 'login');
-                        } else {
-                            $this->core_log->new_error('Imposible guardar los datos de usuario.');
-                            $this->cache->clean();
-                        }
-                    } else {
-                        $this->core_log->new_error('¡Contraseña incorrecta! (' . $nick . ')');
-                        $this->core_log->save('¡Contraseña incorrecta! (' . $nick . ')', 'login', TRUE);
-                        $this->ip_filter->setAttempt($ip);
-                    }
-                } else if ($user && !$user->enabled) {
-                    $this->core_log->new_error('El usuario ' . $user->nick . ' está desactivado, habla con tu administrador!');
-                    $this->core_log->save('El usuario ' . $user->nick . ' está desactivado, habla con tu administrador!', 'login', TRUE);
-                    $this->user_model->clean_cache(TRUE);
-                    $this->cache->clean();
-                } else {
-                    $this->core_log->new_error('El usuario o contraseña no coinciden!');
-                    $this->user_model->clean_cache(TRUE);
-                    $this->cache->clean();
-                }
+            if (FS_DEMO) {
+                /// en el modo demo nos olvidamos de la contraseña
+                return $this->log_in_demo($controller_user, $nick);
             }
-        } else if (filter_input(INPUT_COOKIE, 'user') && filter_input(INPUT_COOKIE, 'logkey')) {
-            $nick = filter_input(INPUT_COOKIE, 'user');
-            $logkey = filter_input(INPUT_COOKIE, 'logkey');
 
-            $user = $this->user_model->get($nick);
-            if ($user && $user->enabled) {
-                if ($user->log_key == $logkey) {
-                    $user->logged_on = TRUE;
-                    $user->update_login();
-                    $this->save_cookie($user);
-                    $controller_user = $user;
-                } else if (!is_null($user->log_key)) {
-                    $this->core_log->new_message('¡Cookie no válida! Alguien ha accedido a esta cuenta desde otro PC con IP: '
-                        . $user->last_ip . ". Si has sido tú, ignora este mensaje.");
-                    $this->log_out();
-                }
-            } else {
-                $this->core_log->new_error('¡El usuario ' . $nick . ' no existe o está desactivado!');
-                $this->log_out(TRUE);
-                $this->user_model->clean_cache(TRUE);
-                $this->cache->clean();
-            }
+            $this->ip_filter->set_attempt($ip);
+            return $this->log_in_user($controller_user, $nick, $password, $ip);
         }
 
-        return $controller_user->logged_on;
-    }
-
-    private function login_demo(&$controller_user, $email)
-    {
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $aux = explode('@', $email);
-            $nick = substr($aux[0], 0, 12);
-            if ($nick == 'admin') {
-                $nick .= $this->random_string(7);
-            }
-
-            $user = $this->user_model->get($nick);
-            if (!$user) {
-                $user = new fs_user();
-                $user->nick = $nick;
-                $user->set_password('demo');
-                $user->email = $email;
-
-                /// creamos un agente para asociarlo
-                $agente = new agente();
-                $agente->codagente = $agente->get_new_codigo();
-                $agente->nombre = $nick;
-                $agente->apellidos = 'Demo';
-                $agente->email = $email;
-
-                if ($agente->save()) {
-                    $user->codagente = $agente->codagente;
-                }
-            }
-
-            $user->new_logkey();
-            if ($user->save()) {
-                $this->save_cookie($user);
-                $controller_user = $user;
-            }
-        } else {
-            $this->core_log->new_error('Email no válido');
+        if (filter_input(INPUT_COOKIE, 'user') && filter_input(INPUT_COOKIE, 'logkey')) {
+            return $this->log_in_cookie($controller_user);
         }
+
+        return FALSE;
     }
 
     /**
@@ -227,6 +185,148 @@ class fs_login
         $this->core_log->save('El usuario ha cerrado la sesión.', 'login');
     }
 
+    /**
+     * 
+     * @param fs_user $controller_user
+     *
+     * @return bool
+     */
+    private function log_in_cookie(&$controller_user)
+    {
+        $nick = filter_input(INPUT_COOKIE, 'user');
+        $user = $this->user_model->get($nick);
+        if ($user && $user->enabled) {
+            $logkey = filter_input(INPUT_COOKIE, 'logkey');
+            if ($user->log_key == $logkey) {
+                $user->logged_on = TRUE;
+                $user->update_login();
+                $this->save_cookie($user);
+                $controller_user = $user;
+            } else if (!is_null($user->log_key)) {
+                $this->core_log->new_message('¡Cookie no válida! Alguien ha accedido a esta cuenta desde otro PC con IP: '
+                    . $user->last_ip . ". Si has sido tú, ignora este mensaje.");
+                $this->log_out();
+            }
+        } else {
+            $this->core_log->new_error('¡El usuario ' . $nick . ' no existe o está desactivado!');
+            $this->log_out(TRUE);
+            $this->user_model->clean_cache(TRUE);
+            $this->cache->clean();
+        }
+
+        return $controller_user->logged_on;
+    }
+
+    /**
+     * 
+     * @param fs_user $controller_user
+     * @param string  $email
+     *
+     * @return bool
+     */
+    private function log_in_demo(&$controller_user, $email)
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->core_log->new_error('Email no válido: ' . $email);
+            return FALSE;
+        }
+
+        $aux = explode('@', $email);
+        $nick = substr($aux[0], 0, 12);
+        if ($nick == 'admin') {
+            $nick .= $this->random_string(7);
+        }
+
+        $user = $this->user_model->get($nick);
+        if (!$user) {
+            $user = new fs_user();
+            $user->nick = $nick;
+            $user->set_password('demo');
+            $user->email = $email;
+
+            /// creamos un agente para asociarlo
+            $agente = new agente();
+            $agente->codagente = $agente->get_new_codigo();
+            $agente->nombre = $nick;
+            $agente->apellidos = 'Demo';
+            $agente->email = $email;
+
+            if ($agente->save()) {
+                $user->codagente = $agente->codagente;
+            }
+        }
+
+        $user->new_logkey();
+        if ($user->save()) {
+            $this->save_cookie($user);
+            $controller_user = $user;
+        }
+
+        return $controller_user->logged_on;
+    }
+
+    /**
+     * 
+     * @param fs_user $controller_user
+     * @param string  $nick
+     * @param string  $password
+     * @param string  $ip
+     *
+     * @return boolean
+     */
+    private function log_in_user(&$controller_user, $nick, $password, $ip)
+    {
+        $user = $this->user_model->get($nick);
+        if (!$user) {
+            $this->core_log->new_error('El usuario o contraseña no coinciden!');
+            $this->user_model->clean_cache(TRUE);
+            $this->cache->clean();
+            return FALSE;
+        }
+
+        if (!$user->enabled) {
+            $this->core_log->new_error('El usuario ' . $user->nick . ' está desactivado, habla con tu administrador!');
+            $this->core_log->save('El usuario ' . $user->nick . ' está desactivado, habla con tu administrador!', 'login', TRUE);
+            $this->user_model->clean_cache(TRUE);
+            $this->cache->clean();
+            return FALSE;
+        }
+
+        /**
+         * En versiones anteriores se guardaban las contraseñas siempre en
+         * minúsculas, por eso, para dar compatibilidad comprobamos también
+         * en minúsculas.
+         */
+        if ($user->password != sha1($password) && $user->password != sha1(mb_strtolower($password, 'UTF8'))) {
+            $this->core_log->new_error('¡Contraseña incorrecta! (' . $nick . ')');
+            $this->core_log->save('¡Contraseña incorrecta! (' . $nick . ')', 'login', TRUE);
+            return FALSE;
+        }
+
+        $user->new_logkey();
+
+        if (!$user->admin && !$this->ip_filter->in_white_list($ip)) {
+            $this->core_log->new_error('No puedes acceder desde esta IP.');
+            $this->core_log->save('No puedes acceder desde esta IP.', 'login', TRUE);
+        } else if ($user->save()) {
+            $this->save_cookie($user);
+
+            /// añadimos el mensaje al log
+            $this->core_log->save('Login correcto.', 'login');
+
+            $controller_user = $user;
+            return $controller_user->logged_on;
+        }
+
+        $this->core_log->new_error('Imposible guardar los datos de usuario.');
+        $this->cache->clean();
+        return FALSE;
+    }
+
+    /**
+     * 
+     * @param fs_user $user
+     */
     private function save_cookie($user)
     {
         setcookie('user', $user->nick, time() + FS_COOKIES_EXPIRE);
@@ -235,7 +335,9 @@ class fs_login
 
     /**
      * Devuelve un string aleatorio de longitud $length
+     *
      * @param integer $length la longitud del string
+     *
      * @return string la cadena aleatoria
      */
     private function random_string($length = 30)
