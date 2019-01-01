f35628b490ffc91052d1ef0b7e9e4d30dbf29e46
NeoRazorX <neorazorx@gmail.com>
2019-01-01 20:25:29 +0100

    Pequeña refactorización de la API para añadir la función lastactivity.

--- api.php
+++ api.php
@@ -27,37 +27,16 @@ $db = new fs_db2();
 
 require_once 'base/fs_extended_model.php';
 require_once 'base/fs_log_manager.php';
+require_once 'base/fs_api.php';
 require_all_models();
 
 $db->connect();
 
-if (!$db->connected()) {
-    echo 'ERROR al conectar a la base de datos';
-} else if (!fs_filter_input_req('v')) {
-    echo 'Version de la API de FacturaScripts ausente. Actualiza el cliente.';
-} else if (fs_filter_input_req('v') != '2') {
-    echo 'Version de la API de FacturaScripts incorrecta. Actualiza el cliente.';
-} else if (fs_filter_input_req('f')) {
-    $ejecutada = FALSE;
-    $fsext = new fs_extension();
-    foreach ($fsext->all_4_type('api') as $ext) {
-        if ($ext->text == fs_filter_input_req('f')) {
-            try {
-                call_user_func(fs_filter_input_req('f'));
-            } catch (Exception $exception) {
-                echo 'ERROR: ' . $exception->getMessage();
-            }
-
-            $ejecutada = TRUE;
-            break;
-        }
-    }
-
-    if (!$ejecutada) {
-        echo 'Ninguna funcion API ejecutada.';
-    }
+if ($db->connected()) {
+    $api = new fs_api();
+    echo $api->run();
 } else {
-    echo 'Ninguna funcion ejecutada.';
+    echo 'ERROR al conectar a la base de datos';
 }
 
 /// guardamos los errores en el log
